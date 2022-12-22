fb15573335da4cc462542d5e24414c850dccbf61
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-12-22 11:06:14 +0700

    Commit 3
    Task Button_Set
    Add logic to button click

--- l1_button_set/button_set/MainWindow.xaml.cs
+++ l1_button_set/button_set/MainWindow.xaml.cs
@@ -24,5 +24,90 @@ namespace button_set
         {
             InitializeComponent();
         }
+        private void buttonNavy_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Navy";
+            this.background.Background = new SolidColorBrush(Colors.Navy);
+        }
+        private void buttonBlue_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Blue";
+            this.background.Background = new SolidColorBrush(Colors.Blue);
+        }
+        private void buttonAqua_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Aqua";
+            this.background.Background = new SolidColorBrush(Colors.Aqua);
+        }
+        private void buttonTeal_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Teal";
+            this.background.Background = new SolidColorBrush(Colors.Teal);
+        }
+        private void buttonOlive_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Olive";
+            this.background.Background = new SolidColorBrush(Colors.Olive);
+        }
+        private void buttonGreen_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Green";
+            this.background.Background = new SolidColorBrush(Colors.Green);
+        }
+        private void buttonLime_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Lime";
+            this.background.Background = new SolidColorBrush(Colors.Lime);
+        }
+        private void buttonYellow_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Yellow";
+            this.background.Background = new SolidColorBrush(Colors.Yellow);
+        }
+        private void buttonOrange_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Orange";
+            this.background.Background = new SolidColorBrush(Colors.Orange);
+        }
+        private void buttonRed_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Red";
+            this.background.Background = new SolidColorBrush(Colors.Red);
+        }
+        private void buttonMaroon_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Maroon";
+            this.background.Background = new SolidColorBrush(Colors.Maroon);
+        }
+        private void buttonFuchsia_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Fuchsia";
+            this.background.Background = new SolidColorBrush(Colors.Fuchsia);
+        }
+        private void buttonPurple_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Purple";
+            this.background.Background = new SolidColorBrush(Colors.Purple);
+        }
+        private void buttonBlack_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Black";
+            this.background.Background = new SolidColorBrush(Colors.Black);
+        }
+        private void buttonSilver_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Silver";
+            this.background.Background = new SolidColorBrush(Colors.Silver);
+        }
+        private void buttonGray_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Gray";
+            this.background.Background = new SolidColorBrush(Colors.Gray);
+        }
+        private void buttonWhite_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "White";
+            this.background.Background = new SolidColorBrush(Colors.White);
+        }
     }
 }
