d1aa8d2eec2f75a53b7958da0ff1d3bf7c6e943d
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-12-25 17:55:28 +0700

    Commit 14
    Task Reg_Window:
    add logic to reg button

--- l4_reg_window/reg_window/MainWindow.xaml.cs
+++ l4_reg_window/reg_window/MainWindow.xaml.cs
@@ -24,5 +24,61 @@ namespace reg_window
         {
             InitializeComponent();
         }
+
+        private void regButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (loginBox.Text.Length < 4)
+            {
+                loginBox.ToolTip = "Слишком короткий логин";
+                loginBox.Background = Brushes.IndianRed;
+            }
+            else if (passwordBox1.Password.Length < 6)
+            {
+                byDefault(loginBox, 'l');
+                passwordBox1.ToolTip = "Слишком короткий пароль";
+                passwordBox1.Background = Brushes.IndianRed;
+            }
+            else if(passwordBox1.Password != passwordBox2.Password)
+            {
+                byDefault(passwordBox1, 1);
+                passwordBox2.ToolTip = "Пароли не совпадают";
+                passwordBox2.Background = Brushes.IndianRed;
+            }
+            else if(!emailBox.Text.Contains("@") || !emailBox.Text.Contains(".ru"))
+            {
+                byDefault(passwordBox2, 2);
+                emailBox.ToolTip = "E-Mail введен неправильно";
+                emailBox.Background = Brushes.IndianRed;
+            }
+            else
+            {
+                byDefault(emailBox, 'e');
+            }
+        }
+        private void byDefault(TextBox obj, char name)
+        {
+            if (name == 'l')
+            {
+                obj.ToolTip = "Не менее 4 символов";                
+            }
+            if (name == 'e')
+            {
+                obj.ToolTip = "Только в домене .ru";
+            }
+            obj.Background = Brushes.Transparent;
+        }
+        private void byDefault(PasswordBox obj, byte name)
+        {
+            if (name == 1)
+            {
+                obj.ToolTip = "Не менее 6 символов";
+            }
+            if (name == 2)
+            {
+                obj.ToolTip = "Пароли должны совпадать";
+            }
+            obj.Background = Brushes.Transparent;
+        }
     }
 }
