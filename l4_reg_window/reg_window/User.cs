5d0ddf3d3ec16ffdf717a3bd0fbd96de6193f23b
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-12-25 18:10:23 +0700

    Commit 15
    Task Reg_Window
    create class User

---
+++ l4_reg_window/reg_window/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reg_window
+{
+    internal class User
+    {
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public User() { }
+        public User(string? name, string? password, string? email)
+        {
+            Name = name;
+            Password = password;
+            Email = email;
+        }
+    }
+}
