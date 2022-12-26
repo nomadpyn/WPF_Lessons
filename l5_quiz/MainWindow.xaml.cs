112c6516d59e82a085a83e8accf86438119cce3a
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-12-26 16:37:35 +0700

    Commit 20
    Task Quiz
    Add names in tab
    Create class QuizData
    Create method to add questions to form

--- l5_quiz/MainWindow.xaml.cs
+++ l5_quiz/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace quiz
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<QuizData> data = new List<QuizData>
+        {
+            new QuizData("Кто глава дома Симпсонов?","Гомер","Барни","Мо","Мэгги"),
+            new QuizData("Сколько сезонов на данный момент?","33","1","15","21"),
+            new QuizData("Кем никогда не работал Гомер?","Разработчик","Астронавт","Бармен","Телохранитель"),
+            new QuizData("Как звали рыжего суперзлодея?","Скорпио","Бонд","Золотой Глаз","Бен"),
+            new QuizData("Какого цвета у Мардж машина?","Красная","Синяя","Белая","Желтая"),
+            new QuizData("Кто средний по возрасту из Симпсонов","Барт","Мэгги","Лиза","Гомер")
+        };
         public MainWindow()
         {
             InitializeComponent();
+            for (int i = 0; i < 6; i++)
+            {
+                addDataToTab(i);
+            }
+        }
+        private void addDataToTab(int index)
+        {
+            object wantedTextBlock = tabs.FindName($"tab{index+1}_quest");
+            if(wantedTextBlock is TextBlock)
+            {
+                TextBlock wantedChild = wantedTextBlock as TextBlock;
+                wantedChild.Text = data[index].Question;
+            }
+            int[] arr = new int []{ 0, 1, 2, 3 };
+            Random rnd = new Random();
+            int[] RandomArray = arr.OrderBy(x => rnd.Next()).ToArray();
+            for (int i = 1; i <= 4; i++)
+            {
+                object wantedButton = tabs.FindName($"tab{index+1}_answer{i}");
+                if (wantedButton is Button)
+                {
+                    Button wantedChild = wantedButton as Button;
+                    
+                    wantedChild.Content = data[index].Answers[RandomArray[i-1]];
+                }
+            }
         }
 
     }
---
+++ l5_quiz/QuizData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz
+{
+    public class QuizData
+    {
+        public string? Question { get; set; }
+        public string? [] Answers { get; set; }
+        public QuizData() { }
+        public QuizData(string? question, string? answerCorrect, string? answerIncorrect1, string? answerIncorrect2, string? answerIncorrect3)
+        {
+            Question = question;
+            Answers = new string[] {answerCorrect,answerIncorrect1,answerIncorrect2,answerIncorrect3};
+           
+        }
+    }
+}
